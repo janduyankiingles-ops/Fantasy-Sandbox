fa6cbda5b5046ec3623d21771ee8c794badefe92
janduyankiingles-ops <janduyankiingles@gmail.com>
2026-09-20 13:36:20 -0300

    v0.0.6: sync crafted tools in inventory UI

--- scripts/inventory_ui.gd
+++ scripts/inventory_ui.gd
@@ -2,6 +2,9 @@ extends Control
 
 @onready var wood_count_label: Label = $InventoryPanel/Margin/VBox/Grid/WoodSlot/Margin/VBox/Count
 @onready var stone_count_label: Label = $InventoryPanel/Margin/VBox/Grid/StoneSlot/Margin/VBox/Count
+@onready var axe_label: Label = $InventoryPanel/Margin/VBox/Grid/Empty3/Label
+@onready var pickaxe_label: Label = $InventoryPanel/Margin/VBox/Grid/Empty4/Label
+@onready var sword_label: Label = $InventoryPanel/Margin/VBox/Grid/Empty5/Label
 
 func _ready() -> void:
 	visible = false
@@ -9,6 +12,18 @@ func _ready() -> void:
 func toggle_inventory() -> void:
 	visible = not visible
 
-func refresh_inventory(wood_amount: int, stone_amount: int) -> void:
+func close_inventory() -> void:
+	visible = false
+
+func refresh_inventory(
+	wood_amount: int,
+	stone_amount: int,
+	axe_amount: int,
+	pickaxe_amount: int,
+	sword_amount: int
+) -> void:
 	wood_count_label.text = "Quantidade: %d" % wood_amount
 	stone_count_label.text = "Quantidade: %d" % stone_amount
+	axe_label.text = "Machado\nx%d" % axe_amount
+	pickaxe_label.text = "Picareta\nx%d" % pickaxe_amount
+	sword_label.text = "Espada\nx%d" % sword_amount
