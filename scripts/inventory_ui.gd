45c0763161ee2d08404949cce22e040fac52561f
janduyankiingles-ops <janduyankiingles@gmail.com>
2026-09-20 13:44:01 -0300

    v0.0.7: make inventory display only owned items

--- scripts/inventory_ui.gd
+++ scripts/inventory_ui.gd
@@ -1,13 +1,51 @@
 extends Control
 
-@onready var wood_count_label: Label = $InventoryPanel/Margin/VBox/Grid/WoodSlot/Margin/VBox/Count
-@onready var stone_count_label: Label = $InventoryPanel/Margin/VBox/Grid/StoneSlot/Margin/VBox/Count
-@onready var axe_label: Label = $InventoryPanel/Margin/VBox/Grid/Empty3/Label
-@onready var pickaxe_label: Label = $InventoryPanel/Margin/VBox/Grid/Empty4/Label
-@onready var sword_label: Label = $InventoryPanel/Margin/VBox/Grid/Empty5/Label
+var item_order: Array[String] = [
+	"stick",
+	"small_stone",
+	"vine",
+	"wood",
+	"stone",
+	"improvised_axe",
+	"improvised_pickaxe",
+	"improvised_knife",
+	"axe",
+	"pickaxe",
+	"sword"
+]
+
+var item_names: Dictionary = {
+	"stick": "Graveto",
+	"small_stone": "Pedra Pequena",
+	"vine": "Cipó",
+	"wood": "Madeira",
+	"stone": "Pedra",
+	"improvised_axe": "Machado Improvisado",
+	"improvised_pickaxe": "Picareta Improvisada",
+	"improvised_knife": "Faca Improvisada",
+	"axe": "Machado",
+	"pickaxe": "Picareta",
+	"sword": "Espada"
+}
+
+@onready var slots: Array[PanelContainer] = [
+	$InventoryPanel/Margin/VBox/Grid/Slot1,
+	$InventoryPanel/Margin/VBox/Grid/Slot2,
+	$InventoryPanel/Margin/VBox/Grid/Slot3,
+	$InventoryPanel/Margin/VBox/Grid/Slot4,
+	$InventoryPanel/Margin/VBox/Grid/Slot5,
+	$InventoryPanel/Margin/VBox/Grid/Slot6,
+	$InventoryPanel/Margin/VBox/Grid/Slot7,
+	$InventoryPanel/Margin/VBox/Grid/Slot8,
+	$InventoryPanel/Margin/VBox/Grid/Slot9,
+	$InventoryPanel/Margin/VBox/Grid/Slot10,
+	$InventoryPanel/Margin/VBox/Grid/Slot11,
+	$InventoryPanel/Margin/VBox/Grid/Slot12
+]
 
 func _ready() -> void:
 	visible = false
+	_clear_slots()
 
 func toggle_inventory() -> void:
 	visible = not visible
@@ -15,15 +53,29 @@ func toggle_inventory() -> void:
 func close_inventory() -> void:
 	visible = false
 
-func refresh_inventory(
-	wood_amount: int,
-	stone_amount: int,
-	axe_amount: int,
-	pickaxe_amount: int,
-	sword_amount: int
-) -> void:
-	wood_count_label.text = "Quantidade: %d" % wood_amount
-	stone_count_label.text = "Quantidade: %d" % stone_amount
-	axe_label.text = "Machado\nx%d" % axe_amount
-	pickaxe_label.text = "Picareta\nx%d" % pickaxe_amount
-	sword_label.text = "Espada\nx%d" % sword_amount
+func refresh_inventory(snapshot: Dictionary) -> void:
+	_clear_slots()
+
+	var slot_index: int = 0
+
+	for index in range(item_order.size()):
+		var item_key: String = item_order[index]
+		var amount: int = int(snapshot.get(item_key, 0))
+
+		if amount <= 0:
+			continue
+		if slot_index >= slots.size():
+			break
+
+		var slot: PanelContainer = slots[slot_index]
+		var label: Label = slot.get_node("Label") as Label
+		var display_name: String = str(item_names.get(item_key, item_key))
+
+		label.text = "%s\nx%d" % [display_name, amount]
+		slot.visible = true
+		slot_index += 1
+
+func _clear_slots() -> void:
+	for index in range(slots.size()):
+		var slot: PanelContainer = slots[index]
+		slot.visible = false
