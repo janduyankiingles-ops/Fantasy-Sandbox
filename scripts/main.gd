84e4403f68dcb66b1e7fe259217886ade2949dc6
janduyankiingles-ops <janduyankiingles@gmail.com>
2026-09-20 14:05:30 -0300

    v0.0.10: show player progression text

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var position_label: Label = $HUD/Panel/Margin/VBox/PositionLabel
 @onready var state_label: Label = $HUD/Panel/Margin/VBox/StateLabel
 @onready var health_label: Label = $HUD/Panel/Margin/VBox/HealthLabel
+@onready var progression_label: Label = $HUD/Panel/Margin/VBox/ProgressionLabel
 @onready var inventory_label: Label = $HUD/Panel/Margin/VBox/InventoryLabel
 @onready var interaction_panel: PanelContainer = $HUD/InteractionPanel
 @onready var interaction_label: Label = $HUD/InteractionPanel/Margin/InteractionLabel
@@ -56,6 +57,7 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _process(_delta: float) -> void:
 	health_label.text = str(player.call("get_health_text"))
+	progression_label.text = str(player.call("get_progression_text"))
 	position_label.text = "Posição: %d, %d" % [
 		roundi(player.global_position.x),
 		roundi(player.global_position.y)
